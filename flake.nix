56287ffcd3195f1cd9e53b6b8acf44ca5109fce5
Conner Ow <connerow1115@gmail.com>
2023-11-22 17:00:57 -0600

    flake attempt

--- flake.nix
+++ flake.nix
@@ -2,9 +2,15 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
-    devShells.default = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
-      buildInputs = [ postgresql just bun ];
-      # Add other dependencies or settings if needed
+    devShells = {
+      aarch64-darwin = with nixpkgs.legacyPackages.aarch64-darwin; mkShell {
+        buildInputs = [ postgresql just bun ];
+        # Add other dependencies or settings if needed for Darwin
+      };
+      x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
+        buildInputs = [ postgresql just bun ];
+        # Add other dependencies or settings if needed for Linux
+      };
     };
   };
 }
