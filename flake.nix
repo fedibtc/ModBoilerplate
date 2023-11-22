feddc6856b60ac2dae4e7f889618faf84d0df7cb
Conner Ow <connerow1115@gmail.com>
2023-11-22 16:56:44 -0600

    Lightning Utilities, Nix migration

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
-    devShells.default = pkgs.mkShell {
-      buildInputs = [ pkgs.postgresql pkgs.just pkgs.bun ];
-      # Add other dependencies here
+    devShells.default = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
+      buildInputs = [ postgresql just bun ];
+      # Add other dependencies or settings if needed
     };
   };
 }
