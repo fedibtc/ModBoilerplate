04c181ad203967cf3ee919f7f8495d273c5776ca
Conner Ow <connerow1115@gmail.com>
2024-05-14 10:17:13 -0500

    unfree nix package

--- flake.nix
+++ flake.nix
@@ -8,7 +8,10 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let pkgs = import nixpkgs { 
+        inherit system; 
+        config.allowUnfree = true;
+      };
       in {
         devShells = {
           default = pkgs.mkShell {
